34bd54a7390840e343df8d3520430eae14175747
nmccollum <nmccollum@asc.edu>
2014-10-14 09:05:54 -0500

    Make the script more readable
    More readable plus folding text for wider terminals.

--- real-mem.sh
+++ real-mem.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
-COLUMN=$(tput cols)
-INSTMOD=$(dmidecode -t 17 | grep "Size:" | sed 's/^[ \t]*//;s/[ \t]*$//' | sed -e 's/Size: //g')
-NODEMEM=`free | grep Mem | awk '{print $2}'`
-f () { echo "scale=20; a=($1); scale=0; b=a/1; if (a==b) b else b+1" | bc -l; }
-NODEMEMGB=`f "$NODEMEM / ( 1024 * 1024 )"`
-PHYRAM=$(dmidecode -t 17 | grep "Size:" | sed 's/^[ \t]*//;s/[ \t]*$//' | sed -e 's/Size: //g' | grep MB | awk '{s+=$1} END {print s}')
-RAMAVAIL=$(free -m | grep Mem | awk '{print $2}')
+COLUMNS=$(tput cols)
+if [[ $COLUMNS > "120" ]]; then
+        COLUMNS="120"
+fi
 
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' "_"
-printf "This script will calculate the amount of RAM currently in the system using dmidecode and calculate how much RAM the linux kernel can see.  Hopefully this will help you determine if a system is missing RAM.  -Nick \n"
+# I like pretty lines
+printf '%*s\n' "${COLUMNS}" '' | tr ' ' "_"
+printf "This script will calculate the amount of RAM currently in the system using dmidecode and calculate how much RAM the linux kernel can see.  Hopefully this will help you determine if a system is missing RAM.  -Nick \n" | fold -s -w $COLUMNS
 
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' "_"
+# This will print what size stick is in each slot on the motherboard
+printf '%*s\n' "${COLUMNS}" '' | tr ' ' "_"
 printf "What stick of RAM is in each slot?\n"
+INSTMOD=$(dmidecode -t 17 | grep "Size:" | sed 's/^[ \t]*//;s/[ \t]*$//' | sed -e 's/Size: //g')
 printf "$INSTMOD\n" | while read line; do
         SLOT=$((SLOT+1))
         printf "Slot $SLOT: $line\n"
-done | column -c $COLUMN
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' "_"
+done | column -c $COLUMNS
+
+# Do some math to see how much is installed versus how much the kernel sees
+printf '%*s\n' "${COLUMNS}" '' | tr ' ' "_"
+RAMAVAIL=$(free -m | grep Mem | awk '{print $2}')
+PHYRAM=$(dmidecode -t 17 | grep "Size:" | sed 's/^[ \t]*//;s/[ \t]*$//' | sed -e 's/Size: //g' | grep MB | awk '{s+=$1} END {print s}')
 printf "According to my caclulations, the machine should have a total of $PHYRAM MB installed...\n"
-printf "Currently the linux kernel see's $RAMAVAIL MB available, for a difference of $(( PHYRAM - $RAMAVAIL)) MB.\n"
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' "_"
+printf "Currently the linux kernel see's $RAMAVAIL MB available, for a difference of $(( PHYRAM - $RAMAVAIL)) MB. There is normally a difference in how much is available versus how much is installed.  The linux kernel dedicates a certain amount of RAM to itself that cannot be used for anything else.\n" | fold -s -w $COLUMNS
+
+# Tell the user if they are missing ram or not
+printf '%*s\n' "${COLUMNS}" '' | tr ' ' "_"
 if [[ $((PHYRAM - $RAMAVAIL)) > "500" ]]; then
         printf "Conclusion? \t Looks like you have some RAM missing!\n"
         else
-        printf "Conclusion? \t Looks like you're good!"
+        printf "Conclusion? \t Looks like you're good!\n"
 fi
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' "_"
+printf '%*s\n' "${COLUMNS}" '' | tr ' ' "_"
